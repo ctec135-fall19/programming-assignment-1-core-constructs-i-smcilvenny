101c37a7f06983003b9ef18448c808b1bd4ff05c
smcilvenny <s.mcilvenny@students.clark.edu>
2019-10-20 23:52:35 -0700

    I dont know how to go about problem 3

--- CS_Core_Constructs_I/Prob1/Program.cs
+++ CS_Core_Constructs_I/Prob1/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
-Author: <your name>
-Date:   <date>
+Author: Sean McIlvenny
+Date:   10/19/19
 CTEC 135: Microsoft Software Development with C#
 
 <Solution Name>
@@ -38,9 +38,43 @@ namespace Prob1
             #region example of a code region
             // some code
             #endregion
-
+            //variable declaration
+            double dbl;
+            double dbl = 1.123;
+            dbl2 = new double(1.122);
             #region another code region
             // some code
+            #endregion
+
+            #region
+            //variable initialization
+            bool t = true;
+            byte myByte = 14;
+            short myShort = 12200;
+            int myInt = 99999;
+            long myLong = 78944;
+            float myFloat = 543.12;
+            double myDbl = 12.00001;
+            decimal myDec = 10.01010;
+
+            Console.WriteLine("True or False: {0}",t);
+            Console.WriteLine("byte: {0}", myByte);
+            Console.WriteLine("short: {0}", myShort);
+            Console.WriteLine("int: {0}", myInt);
+            Console.WriteLine("long: {0}", myLong);
+            Console.WriteLine("float to currency: {0:c}", myFloat);
+            Console.WriteLine("double to decimal: {0:d}", myDbl);
+            Console.WriteLine("decimal to fixed point: {0:2f}", myDec);
+            #endregion
+
+            #region
+            //Max Min Values
+            byte max = 255;
+            byte min = 0;
+            Console.WriteLine("The max value of a byte is: {0}", max);
+            Console.WriteLine("The min value of a byte is: {0}", min);
+
+
             #endregion
         }
     }
---
+++ Prob2/Prob2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            int i;
+            for (i = 1; i <= 5; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            //while loop
+            int x = 1;
+            while(x <= 5)
+            {
+                Console.Write("{0} ", x);
+                x++;
+            }
+            Console.WriteLine();
+
+            //do while loop
+            int y = 1;
+            do
+            {
+                Console.Write("{0} ", y);
+                y++;
+            } while (y <= 5);
+
+            Console.WriteLine();
+        }
+    }
+}
